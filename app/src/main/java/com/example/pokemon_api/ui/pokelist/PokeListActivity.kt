bf17f1298d70812c91efdbab33bfc955d4f860c2
JCBR2609 <juan_carlos_2609@hotmail.com>
2021-10-06 17:46:21 -0500

    ListaCompleta

---
+++ app/src/main/java/com/example/pokemon_api/ui/pokelist/PokeListActivity.kt
@@ -0,0 +1,67 @@
+package com.example.pokemon_api.ui.pokelist
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.pokemon_api.R
+import com.example.pokemon_api.ui.pokeinfo.PokeInfoActivity
+import kotlinx.android.synthetic.main.activity_pokelist.*
+
+class PokeListActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: PokeListViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokelist)
+
+        viewModel = ViewModelProvider(this).get(PokeListViewModel::class.java)
+
+        initUI()
+    }
+
+    private fun initUI(){
+        pokelistRecyclerView.layoutManager = LinearLayoutManager(this)
+        pokelistRecyclerView.adapter = PokeListAdapter{
+            val intent = Intent(this, PokeInfoActivity::class.java)
+            intent.putExtra("id", it)
+            startActivity(intent)
+        }
+
+        viewModel.getPokemonList()
+
+        viewModel.pokemonList.observe(this, Observer { list ->
+            (pokelistRecyclerView.adapter as PokeListAdapter).setData(list)
+        })
+    }
+}
+   /* private lateinit var viewModel: PokeListViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokelist)
+
+        viewModel= ViewModelProvider(this).get(PokeListViewModel::class.java)
+
+        initUI()
+    }
+
+        private fun initUI(){
+            pokelistRecyclerView.layoutManager = LinearLayoutManager(this)
+            pokelistRecyclerView.adapter = PokeListAdapter{
+                val intent = Intent(this, PokeInfoActivity::class.java)
+                intent.putExtra("id", it)
+                startActivity(intent)
+            }
+
+
+        viewModel.getPokemonList()
+
+        viewModel.pokemonList.observe(this, Observer { list ->
+            (pokelistRecyclerView.adapter as PokeListAdapter).setData(list)
+        })
+    }
+}*/
---
+++ app/src/main/java/com/example/pokemon_api/ui/pokelist/PokeListViewModel.kt
@@ -0,0 +1,69 @@
+package com.example.pokemon_api.ui.pokelist
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.pokemon_api.model.api.PokeApiResponse
+import com.example.pokemon_api.model.api.PokeResult
+import com.example.pokemon_api.service.PokeApiService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class PokeListViewModel(): ViewModel() {
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://pokeapi.co/api/v2/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val service: PokeApiService = retrofit.create(PokeApiService::class.java)
+
+    val pokemonList = MutableLiveData<List<PokeResult>>()
+
+    fun getPokemonList(){
+        val call = service.getPokemonList(100,0)
+
+        call.enqueue(object : Callback<PokeApiResponse>{
+            override fun onResponse(call: Call<PokeApiResponse>,response: Response<PokeApiResponse>) {
+                response.body()?.results?.let { list ->
+                    pokemonList.postValue(list)
+                }
+
+            }
+
+            override fun onFailure(call: Call<PokeApiResponse>, t: Throwable) {
+                call.cancel()
+            }
+
+        })
+    }
+}
+
+/* private val retrofit = Retrofit.Builder()
+        .baseUrl("https://pokeapi.co/api/v2/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val service: PokeApiService = retrofit.create(PokeApiService::class.java)
+    val pokemonList = MutableLiveData<List<PokeResult>>()
+
+    fun getPokemonList(){
+        val call= service.getPokemonList(100,0)
+
+        call.enqueue(object :Callback<PokeApiResponse>{
+            override fun onResponse(call: Call<PokeApiResponse>,response: Response<PokeApiResponse>) {
+                response.body()?.results?.let{ list ->
+                    pokemonList.postValue(list)
+
+                }
+            }
+
+            override fun onFailure(call: Call<PokeApiResponse>, t: Throwable) {
+                call.cancel()
+            }
+        })
+
+
+    }
+}*/
